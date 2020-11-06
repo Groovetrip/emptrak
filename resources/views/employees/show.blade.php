00bc2f3e9972cbae703307cf2910b4abfd70cb32
Chad Ackley <chackley42@gmail.com>
2020-11-05 20:22:03 -0700

    Custom Fields added (Still needs a save button)

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -1,20 +1,61 @@
 @extends('layouts.app')
+@extends('layouts.detail')
 
 @section('content')
 
     <div class = "container">
-        <div class ="header">
-            <h1 class = "mb-4 ">{{ $employee->full_name}}</h1>
-        </div>
+            <div class ="header">
+                <h1 class = "mb-4 " style = "text-align: center;">{{ $employee->full_name}}</h1>
+            </div>
+        <div class = "flex-container">
+            <div class = "static-info">
+                <div class = "grid-container">
+                    <div class = "grid-item">Id</div>
+                    <div class = "grid-item">{{$employee->id}}</div>
+                    <div class = "grid-item">Full Name</div>
+                    <div class = "grid-item">{{$employee->full_name}}</div>
+                    <div class = "grid-item">Email</div>
+                    <div class = "grid-item">{{$employee->email}}</div>
+                    <div class = "grid-item">Phone</div>
+                    <div class = "grid-item">{{$employee->phone}}</div>
+                    <div class = "grid-item">Gender</div>
+                    <div class = "grid-item">{{ucfirst($employee->gender)}}</div>
+                    <div class = "grid-item">Birth Date</div>
+                    <div class = "grid-item">{{$employee->birth_date->format('M d Y')}}</div>
+                </div>
+            </div>
+                
 
-        <div class = "static-info">
+            <div class = "custom-fields">
+                <div class = "grid-container_2">
+                    <div class = "grid-item_2">Payment Type</div>
+                    <div class = "grid-item_2">
+                        <form>
+                            <input type = "text" id = "ptype" name = "paytype">
+                        </form>
+                    </div> 
+                    <div class = "grid-item_2">Amount of Pay</div>
+                    <div class = "grid-item_2">
+                        <form>
+                            <input type = "text" id = "aopay" name = "amount">
+                        </form>
+                    </div>
+                    <div class = "grid-item_2">Department</div>
+                    <div class = "grid-item_2">
+                        <form>
+                            <input type = "text" id = "dep" name = "department">
+                        </form>
+                    </div>
+                    <div class = "grid-item_2">Comments</div>
+                    <div class = "grid-item_2">
+                        <form>
+                            <textarea rows = "4" cols = "50" name = "comments" >
+                            </textarea>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div class = "custom-fields">
-        </div>
-
     </div>
-
-
 
 @endsection
---
+++ resources/views/layouts/detail.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link href="style.css" rel="stylesheet" />
+  <style>
+    .grid-container {
+        display: inline-grid;
+        background-color: lightblue;
+        grid-template-columns: auto auto;
+        float: left;
+        margin: 0 1.5%;
+        padding: 10px;
+        border-radius: 25px;
+    }
+    .grid-item {
+        border: 1px solid;
+        text-align: center;
+    }
+    .grid-container_2 {
+        display: inline-grid;
+        background-color: rgb(204, 255, 204);
+        grid-template-columns: auto auto;
+        float: right;
+        margin: 0 1.5%;
+        padding: 10px;
+        border-radius: 25px;
+
+    }
+    .grid-item_2 {
+        border: 1px solid;
+        text-align: center;
+    }
+    .flex-container {
+        display: flex;
+        justify-content: space-around;
+        
+    }
+    input[type=text]{
+        background-color: rgb(230, 255, 230);
+    }
+    textarea[name=comments]{
+        background-color: rgb(230, 255, 230);
+    }
+  </style>
+</head>
+<body>
+
+
+</body>
+</html>
