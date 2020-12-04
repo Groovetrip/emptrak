ca0cd59aa4da227404ca560deeb4f40957e4afc9
Alex Adams <alex@camerashy.com>
2020-12-03 21:13:41 -0700

    Add CreateUser command class

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:user {--email=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $user = User::create([
+            'name' => 'Testy McTestFace',
+            'email' => $this->option('email'),
+            'password' => Hash::make('password'),
+        ]);
+
+        $user->assignRole('Super Admin');
+
+        return 0;
+    }
+}
